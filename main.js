6d062efacd4f8284ee79df3b14f53bbd798753ce
rikmori <101608843+rikmori@users.noreply.github.com>
2022-03-15 21:55:24 +0800

    bot

---
+++ main.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+
+
+client.once('ready', () => {
+    console.log('Rikbot is online!');
+    client.user.setActivity('Children', { type: 'WATCHING'});
+});
+
+client.on('message', msg=>{
+    if(msg.content ===  "geisen"){
+        msg.reply('Mmmkay folks.');
+    }
+})
+
+client.on('message', msg=>{
+    if(msg.content ===  "laptop"){
+        msg.reply('Mmmkay folks you can go ahead and shut your laptops down for me please.');
+    }
+})
+
+client.on('message', msg=>{
+    if(msg.content ===  "fuck"){
+        msg.reply('We are actually not gonna have you say that word right now.');
+    }
+})
+
+client.on('message', msg=>{
+    if(msg.content ===  "cya"){
+        msg.reply('Mmmkay folks, its that magical time of day.');
+    }
+})
+
+
+client.login(process.env.token);
